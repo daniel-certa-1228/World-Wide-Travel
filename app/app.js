18b95af402e36b2d441f74ea9c88bf025fdd30a3
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-31 20:36:43 -0500

    DONE - MVP

--- app/app.js
+++ app/app.js
@@ -2,3 +2,12 @@
 console.log( "app.js" );
 
 var app = angular.module("BookStoreApp", ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider.
+	when('/', {
+		templateUrl: 'partials/book-list.html',
+		controller: 'BookCtrl'
+	}).
+	otherwise('/');
+});
--- app/factories/bookFactory.js
+++ app/factories/bookFactory.js
@@ -6,11 +6,11 @@ app.factory("BookFactory", function($q,$http) {
 	let getBooks = () => {
 		let books = [];
 		return $q ((resolve, reject) => {
-			$http.get('.data/guides.json')
+			$http.get('./data/guides.json')
 			.then((bookObject) => {
-				let bookCollection = bookObject;
-				resolve(bookCollection.guides);
-				console.log( "bookCollection", bookCollection.guides );
+				let bookCollection = bookObject.data.guides;
+				resolve(bookCollection);
+				console.log( "bookCollection", bookCollection );
 			})
 			.catch((error) => {
 				reject(error);
